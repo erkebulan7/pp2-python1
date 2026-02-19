4edd71f763db5f4ef146f3d6af18e9e55e39dbaa
Yerkebulan <berdialyerkebulan@gmail.com>
2026-02-20 00:44:20 +0500

    adding cities

---
+++ Practice_3/functions/function_arguments.py
@@ -0,0 +1,11 @@
+def introduce(name, city):
+    print(f"My name is {name} and I live in {city}")
+
+introduce("Aruzhan", "Almaty")
+introduce(name="Dias", city="Shymkent")
+
+def welcome(name="Aigerim"):
+    print(f"Welcome, {name}")
+
+welcome()
+welcome("Nurlan")
