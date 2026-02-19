85f6fe61e591e2a19f7972ef2d444f04cf3ab91d
Yerkebulan <berdialyerkebulan@gmail.com>
2026-02-20 00:53:04 +0500

    subtracting function

---
+++ Practice_3/functions/return_values.py
@@ -0,0 +1,13 @@
+def subtract(a, b):
+    return a - b
+
+def person():
+    name = "Sanzhar"
+    city = "Astana"
+    return name, city
+
+print(subtract(10, 3))
+
+n, c = person()
+print(n)
+print(c)
